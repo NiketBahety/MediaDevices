e6d5ced0b502fa3f8d480f251062f39bc38017b6
NiketBahety <niketbahety@gmail.com>
2024-01-24 16:13:36 +0530

    2

--- client.js
+++ client.js
@@ -1,14 +1,24 @@
 let cam = document.getElementById("cam");
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
 if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices
     .getUserMedia({ video: true })
     .then(function (s) {
-      console.log(s);
       cam.srcObject = s;
+      cam.addEventListener("play", function () {
+        draw(this);
+      });
     })
     .catch((err) => {
       alert("You need to allow cam access to use this app!");
     });
 } else {
   console.log("Please allow camera access");
+}
+
+function draw(video) {
+  ctx.drawImage(video, canvas.width, canvas.height);
+  setTimeout(draw, 10, video);
 }
